30f624627281df08e6cefeae201c219b8c6af250
Vladyslav Kharitonov <ramix1235@gmail.com>
2017-05-15 15:04:33 +0000

    work with array in footer

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,5 +1,20 @@
 <div id="footer">
-    Footer content...
+    <?php
+        $arr = array('PHP', 'MySQL', 'Apache');
+        for ($i = 0; $i < count($arr); $i++) 
+        { 
+            echo "$arr[$i]<br/>";
+        }
+        
+        $arrAssotiation = [ "first" => "PHP", 
+             "second" => "MySQL", 
+             "third" => "Apache"
+        ];
+         foreach($arrAssotiation as $key => $value) 
+        { 
+            echo "$key = $value<br/>"; 
+        } 
+    ?>
 </div>
 </body>
 </html>
